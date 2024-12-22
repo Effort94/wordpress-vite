e845327b43bb521122af7e36aacb13c17b7f7a57
Effort94 <wdavey713@gmail.com>
2024-12-22 16:42:47 +0000

    Tidy up

--- wp-content/themes/training/front-page.php
+++ wp-content/themes/training/front-page.php
@@ -2,15 +2,16 @@
     get_header();
 ?>
 
-    <form>
-        <p>Hello</p>
-        <label for="username"> Username:
-            <input id="username" name="username" type="text" required>
-        </label>
-
-        <p>Now to get html and php files to automatically apply</p>
-    </form>
-
+    <div class="landing-page-container">
+        <div class="landing-page-content">
+            <h1 class="landing-heading">Hello World</h1>
+            <p class="landing-description">This site uses docker, wordpress and vite. Pretty cool right?!</p>
+            <ul>
+                <li>Make changes to the styles and watch magic happen</li>
+                <li>Think you have some bad javascript code?  Try running npx eslint</li>
+            </ul>
+        </div>
+    </div>
 <?php
     get_footer();
 ?>
